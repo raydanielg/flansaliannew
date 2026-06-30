f9883ee84175020403aa6ff8c986e277d70ddba2
raydanielg <airezra2+1@gmail.com>
2026-06-30 19:58:32 +0300

    commit

--- routes/web.php
+++ routes/web.php
@@ -91,3 +91,5 @@ Route::prefix('admin')->middleware(['auth', 'admin'])->group(function () {
 
 Route::get('/team', [PageController::class, 'team'])->name('team');
 Route::get('/gallery', [PageController::class, 'gallery'])->name('gallery');
+Route::get('/sitemap', [PageController::class, 'sitemap'])->name('sitemap');
+Route::get('/sitemap.xml', [PageController::class, 'sitemapXml']);
